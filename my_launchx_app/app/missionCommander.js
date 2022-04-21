35107668cf6fe98a5e095ae3e61d431557853f31
hernandez1981 <kathya_lucia@hotmail.com>
2022-04-21 00:28:50 -0600

    Modificando package.json y missionCommander.test.js

---
+++ my_launchx_app/app/missionCommander.js
@@ -0,0 +1,9 @@
+console.log("Creando mi primer proyecto de JS desde cero")
+class MissionCommander {
+    constructor (name) {
+      this.name = name
+    }
+  }
+  
+  // Esta línea nos permite exportar nuestra clase
+  module.exports = MissionCommander
